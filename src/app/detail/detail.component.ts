dc61ac3a65ed2c813452f90237e4b5a74d6deb7b
JITHIN <jithint1997@gmail.com>
2024-08-15 13:11:06 +0530

    4th commit

--- src/app/detail/detail.component.ts
+++ src/app/detail/detail.component.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { OnInit } from '@angular/core';
 import { ApiService } from '../services/api.service';
+import { ToastrService } from 'ngx-toastr';
 
 @Component({
   selector: 'app-detail',
@@ -10,10 +11,10 @@ import { ApiService } from '../services/api.service';
 })
 export class DetailComponent implements OnInit {
   pid: any = ""
-  product:any={}
+  product: any = {}
 
-  constructor(private ar: ActivatedRoute, private api: ApiService) {
-    this.ar.params.subscribe((res:any)=> {
+  constructor(private ar: ActivatedRoute, private api: ApiService, private toastr: ToastrService) {
+    this.ar.params.subscribe((res: any) => {
       console.log(res);
       this.pid = res.id
     })
@@ -30,4 +31,45 @@ export class DetailComponent implements OnInit {
       }
     })
   }
+
+  addWish(product: any) {
+    if (sessionStorage.getItem('token')) {
+      const { id, title, description, price, category, image, rating } = product
+      this.api.addWishListApi({ id, title, description, price, category, image, rating }).subscribe({
+        next: (res: any) => {
+          this.toastr.success("Product Added To Wishlist")
+          this.api.getWishListItemCount()
+        },
+        error: (err: any) => {
+          console.log(err);
+          this.toastr.error(err.error)
+        }
+      })
+    }
+    else {
+      this.toastr.warning("Please Login First")
+    }
+  }
+
+
+  addCart(product: any) {
+    if (sessionStorage.getItem('token')) {
+      const { id, title, price, image } = product
+      this.api.addToCartApi({ id, title, price, image }).subscribe({
+        next: (res: any) => {
+          this.toastr.success(res)
+          this.api.getCartItemCount()
+
+        },
+        error: (err: any) => {
+          console.log(err);
+          this.toastr.error(err.error)
+        }
+      })
+    }
+    else {
+      this.toastr.warning("Please Login First")
+    }
+  }
+
 }
