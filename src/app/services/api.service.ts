994699050080ecd5ce4d772499f0a9240d801269
JITHIN <jithint1997@gmail.com>
2024-08-15 12:41:08 +0530

    third commit

--- src/app/services/api.service.ts
+++ src/app/services/api.service.ts
@@ -8,7 +8,7 @@ import { BehaviorSubject } from 'rxjs';
 export class ApiService {
 
 
-  base_url: any = "https://ekart-server-o7cn.onrender.com"
+  base_url: any = "https://ekart-server-pkxm.onrender.com"
 
 
   wishCountBS = new BehaviorSubject(0)
